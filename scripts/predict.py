04b666a912facedf80bf9985ab2a790a11b48ab3
Prianka Sengupta <puprianka@gmail.com>
2024-08-19 23:47:12 -0500

    Update predict.py to handle all prediction models

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,8 +1,6 @@
-import os
 import sys
 import pickle
 import numpy as np
-from math import sqrt
 
 def main():
     
@@ -12,21 +10,25 @@ def main():
     feat_vec = np.load(np_file).flatten('F')
     
     # Append synth params
-    synth_params = {'AREA': 10, 'GPL_TIMING_DRIVEN': 1, 'PLACE_DENSITY': 0.85, 'CORE_ASPECT_RATIO': 1.0, 'clock_period': 2.5, 'delay_pct': 0.2, 'uncertainty': 0.025}
+    synth_params = {'ABC_MODE': 1, 'GPL_MODE': 1, 'CORE_ASPECT_RATIO': 0.5, 'SYNTH_HIERARCHICAL': 0, 'CORE_UTILIZATION': 50, 
+                'clk_scaling': 1.0, 'uncertainty': 0.05, 'max_fanout': 20, 'max_capacitance': 0.1}
     synth_param_vals = list(synth_params.values())
     
     # load and run pickle models
     pkl_file = sys.argv[2]
     print(pkl_file)
-    slack_pickle_model = pickle.load(open(pkl_file, "rb"))
+    pickle_model = pickle.load(open(pkl_file, "rb"))
     
-    slack_pred = slack_pickle_model.predict(np.append(feat_vec, synth_param_vals).reshape(1, -1))
+    pred = pickle_model.predict(np.append(feat_vec, synth_param_vals).reshape(1, -1))
 
-    if "model_7" in pkl_file:
-        print(f"Predicted TNS: -{sqrt(abs(slack_pred[0]))/1000: 0.4f} ns")
+    if "slack" in pkl_file:
+      result = f"Prediction: {-1*abs(pred[0])/1000: 0.4f}"
+    elif "pwr" in pkl_file:
+      result = f"Prediction: {abs(pred[0])/1000: 0.4f}"
     else:
-        print(f"Predicted TNS: -{sqrt(abs(slack_pred[0])): 0.4f} ns")
+      result = f"Prediction: {abs(pred[0])/1000: 0.4f}"    
     
+    print(result)
     
 if __name__ == '__main__':
     main()
